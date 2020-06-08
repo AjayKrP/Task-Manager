0c3f4ea2a252ed121ae6ba5180e8a9e4b5542746
ajay <23704820+AjayKrP@users.noreply.github.com>
2020-06-08 07:49:13 +0530

    Update Task.js

--- client/src/components/ui/Task.js
+++ client/src/components/ui/Task.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Server extends React.Component {
+export default class Task extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
